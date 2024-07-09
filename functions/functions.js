62f2c1190f18be41e9e2c9fa3eaa59f95bdd4555
jaabir <alxaarith899@gmail.com>
2024-07-09 07:19:48 +0300

    i created folder for functions

---
+++ functions/functions.js
@@ -0,0 +1,15 @@
+
+//Basic function
+function ali(n1,n2){
+    return n1*n2;
+}
+
+console.log('the product is: ',ali(12,5));
+
+//expression function
+const divv = function(n1,n2){return n1/n2;}
+
+console.log('the division of expression function: ',divv(12,2));
+
+
+console.log('the division of expression function: ',divv(24,8));
