dfcf533e6d6247c21f4a86db3c05c06bddb37807
Xaarith Adullahi <121146446+alxaaruth899@users.noreply.github.com>
2024-07-19 07:27:44 +0300

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -13,3 +13,19 @@ console.log('the division of expression function: ',divv(12,2));
 
 
 console.log('the division of expression function: ',divv(24,8));
+
+function myfunc(num){
+            return num*num;
+        } 
+        console.log(myfunc(10));
+        console.log("what");
+
+
+        // Arrow function now
+
+const summ = (num1,num2) => num1+num2;
+
+ console.log('in arrow function: ',summ(12,8));
+
+
+
